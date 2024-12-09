94cae6a745cd6d3e7fd1fc59a5b1c32a9fb5fd5e
Troy Allen <165961938+troycallen@users.noreply.github.com>
2024-12-09 03:19:04 -0500

    Time: 5 ms (95.01%), Space: 18.1 MB (17.9%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -0,0 +1,25 @@
+class OrderedStream:
+    import heapq
+
+    def __init__(self, n: int):
+        self.stream = {}
+        self.ptr = 1
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey] = value
+
+        res = []
+        if idKey > self.ptr:
+            return res
+        
+        while idKey in self.stream:
+            res.append(self.stream[idKey])
+            idKey += 1
+            self.ptr = idKey
+
+        return res
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
