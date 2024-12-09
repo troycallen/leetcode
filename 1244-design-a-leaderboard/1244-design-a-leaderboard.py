b67570bdd6c199a41822d4e2ade5b92cb047a82e
Troy Allen <165961938+troycallen@users.noreply.github.com>
2024-12-08 21:34:35 -0500

    Time: 7 ms (89.58%), Space: 17.8 MB (7.72%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,21 +1,39 @@
-class Leaderboard:
+import heapq
 
+class Leaderboard:
     def __init__(self):
-        self.leaderboard = {}
+        
+        # Initialize a dictionary to keep track of player scores
+        self.playerScores = {}
 
     def addScore(self, playerId: int, score: int) -> None:
-        if playerId in self.leaderboard:
-            self.leaderboard[playerId] += score
-        else:
-            self.leaderboard[playerId] = score
+        
+        # Add the given score to the player's total score
+        self.playerScores[playerId] = self.playerScores.get(playerId, 0) + score
 
     def top(self, K: int) -> int:
-        return sum(sorted(self.leaderboard.values(), reverse = True)[:K])
         
+        # Create a min-heap to keep track of top K scores
+        minHeap = []
+        
+        for score in self.playerScores.values():
+            
+            if len(minHeap) < K:
+                
+                heapq.heappush(minHeap, score)
+            else:
+                
+                if score > minHeap[0]:
+                    
+                    heapq.heappushpop(minHeap, score)
+        
+        # Sum the top K scores
+        return sum(minHeap)
 
     def reset(self, playerId: int) -> None:
-        self.leaderboard[playerId] = 0
-
+        
+        # Reset the player's score to 0
+        self.playerScores[playerId] = 0
 
 # Your Leaderboard object will be instantiated and called as such:
 # obj = Leaderboard()
