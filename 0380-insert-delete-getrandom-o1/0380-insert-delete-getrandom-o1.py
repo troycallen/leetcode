1ea3d6fe5abad0da24aa743308e8c953b995ca3a
Troy Allen <165961938+troycallen@users.noreply.github.com>
2024-12-10 14:35:30 -0500

    Time: 135 ms (47.51%), Space: 57.5 MB (16.76%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -10,18 +10,20 @@ class RandomizedSet:
         if res:
             self.numMap[val] = len(self.numList)
             self.numList.append(val)
+        
         return res
+        
     def remove(self, val: int) -> bool:
         res = val in self.numMap
 
         if res:
-            index = self.numMap[val]
             last = self.numList[-1]
+            index = self.numMap[val]
             self.numList[index] = last
             self.numList.pop()
             self.numMap[last] = index
             del self.numMap[val]
-
+        
         return res
 
     def getRandom(self) -> int:
