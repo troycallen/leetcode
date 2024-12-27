6b9036bbc399ad25574623a9e387d2108a69a700
Troy Allen <165961938+troycallen@users.noreply.github.com>
2024-12-27 17:56:34 -0500

    Time: 163 ms (51.94%), Space: 37.6 MB (57.96%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,17 +1,23 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = []
-        stack = []
-        for p,s in zip(position, speed):
-            pair.append((p,s))
-        
+        pair = [(p,s) for p, s in zip(position, speed)]
         pair.sort(reverse = True)
+        stack = []
 
         for p,s in pair:
+            # appending the time to reach target 
             stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+
+            # multiple cars and this one will catch up and form a fleet
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                # so we have to pop a car in order to merge them into fleet
                 stack.pop()
+            
+        num_cars = len(stack)
+        return num_cars
+
+
+
         
-        return len(stack)
 
         
