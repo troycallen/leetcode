692c1a83ae271c863e0cd1ae094baacef32dd764
Troy Allen <165961938+troycallen@users.noreply.github.com>
2024-12-29 01:20:48 -0500

    Time: 3 ms (85.59%), Space: 19.9 MB (20.83%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -9,7 +9,8 @@ class Solution:
         for i in intervals:
             if min_heap and min_heap[0] <= i[0]:
                 heapq.heappop(min_heap)
-
             heapq.heappush(min_heap, i[1])
         
         return len(min_heap)
+
+        
