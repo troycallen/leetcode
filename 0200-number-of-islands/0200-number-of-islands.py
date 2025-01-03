4bde634c23d0798376eadd636fc3c79393e33fa7
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-03 13:46:34 -0500

    Time: 230 ms (84.53%), Space: 19.7 MB (60.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        ROWS, COLS = len(grid), len(grid[0])
-        islands = 0
+        if not grid:
+            return 0 
 
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r >= ROWS or 
-                c >= COLS or grid[r][c] == "0"
-            ):
-                return
-                
-            grid[r][c] = "0"
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+        
+        rows = len(grid)
+        cols = len(grid[0])
 
-        for r in range(ROWS):
-            for c in range(COLS):
+        num_islands = 0
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == "0":
+                # out of beounds and water check
+                return 
+
+            grid[row][col] = '0'
+
+            dfs(row -1, col)
+            dfs(row +1 , col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
+        
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(r, c)
-                    islands += 1
+                    num_islands += 1
+                    dfs(r,c)
 
-        return islands
+        return num_islands
+
