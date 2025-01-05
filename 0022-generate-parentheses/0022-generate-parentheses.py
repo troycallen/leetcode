565df3436c5670cc18472327adf982134373342a
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-05 16:06:59 -0500

    Time: 0 ms (100%), Space: 18.2 MB (9.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        res = []
-        
-        def backtrack(open_n, closed_n, path):
-            
+        res  = []
 
-            if open_n == closed_n == n:
-                res.append(path)
+        def backtrack(open_n, closed_n, s):
+            if open_n == n and closed_n == n:
+                res.append(s)
                 return
             
-
             if open_n < n:
-                backtrack(open_n + 1, closed_n, path + "(")
-             
-
+                backtrack(open_n + 1, closed_n, s + "(")
+            
             if closed_n < open_n:
-                backtrack(open_n, closed_n + 1, path + ")")
-                
+                backtrack(open_n, closed_n + 1, s + ")")
+            
+        
         backtrack(0, 0, "")
         return res
+
+
+
