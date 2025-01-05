a60d8f870fdca95739aa71de93d95f8f436b424f
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-05 16:40:13 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -3,12 +3,12 @@ class Solution:
         min_heap  = []
 
         for i in nums:
-            #print("HEAP = ", min_heap)
+            print("HEAP = ", min_heap)
             heapq.heappush(min_heap, i)
             if len(min_heap) > k:
                 heapq.heappop(min_heap)
             
             
         
-        #print("final heap =", min_heap)
+        print("final heap =", min_heap)
         return min_heap[0]
