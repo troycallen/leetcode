7ed622dc46043874f7ee3ed054fa1e4e3dbfad2c
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-05 21:20:17 -0500

    Time: 0 ms (100%), Space: 17.7 MB (24.5%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,9 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         str_digits = [str(i) for i in digits]
-        res = int((''.join(str_digits))) + 1
-        str_res = str(res)
-        converted = []
-        for i in str_res:
-            converted.append(int(i))
+        res = str(int((''.join(str_digits))) + 1)
+        converted = [int(i) for i in res]
         return converted
