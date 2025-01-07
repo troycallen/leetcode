3bb112d6380818430357f64942c0606c70027747
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-07 15:38:27 -0500

    Time: 244 ms (60.54%), Space: 20 MB (57.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0 
+        ROWS = len(grid)
+        COLS = len(grid[0])
 
-        
-        rows = len(grid)
-        cols = len(grid[0])
+        directions = [[1,0], [0,1], [0,-1], [-1,0]]
+        islands = 0
 
-        num_islands = 0
+        def dfs(r,c):
+            if r >= ROWS or c >= COLS or r < 0 or c < 0 or grid[r][c] == "0":
+                return
 
-        def dfs(row, col):
-            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == "0":
-                # out of beounds and water check
-                return 
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+            
 
-            grid[row][col] = '0'
-
-            dfs(row -1, col)
-            dfs(row +1 , col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-
-        
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == "1":
-                    num_islands += 1
                     dfs(r,c)
+                    islands += 1
+        
+        return islands
 
-        return num_islands
 
