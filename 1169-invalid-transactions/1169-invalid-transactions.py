4f3cc1cbc3f9380fdec0140f4c12e31f068cfbb8
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-09 17:45:35 -0500

    Time: 45 ms (33.51%), Space: 18.2 MB (37.29%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -2,19 +2,20 @@ class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         invalid = []
 
-        for i, t1 in enumerate(transactions):
-            name, time, amount, city = t1.split(',')
+        for i, t in enumerate(transactions):
+            name, time, amount, city = t.split(',')
 
             if int(amount) > 1000:
-                invalid.append(t1)
+                invalid.append(t)
                 continue
             
-            for j, t2 in enumerate(transactions):
-                if i != j:
-                    name2, time2, amount2, city2 = t2.split(',')
-
-                    if name == name2 and city != city2 and abs(int(time) - int(time2)) <= 60:
-                        invalid.append(t1)
+            for i2, t2 in enumerate(transactions):
+                name2, time2, amount2, city2 = t2.split(',')
+                if i2 != i:
+                    if name == name2 and city != city2 and abs(int(time2) - int(time)) <= 60:
+                        invalid.append(t)
                         break
-
+                    
         return invalid
+
+        
