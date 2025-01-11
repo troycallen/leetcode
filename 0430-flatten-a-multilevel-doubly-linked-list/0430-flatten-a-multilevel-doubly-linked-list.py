9a2c63d14f25025d87f95b200c45fd7e75edc353
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-11 16:29:16 -0500

    Time: 27 ms (98.17%), Space: 18.2 MB (20.34%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -12,34 +12,28 @@ class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-
-        # Initialize variables
+        
         stack = [head]
-        prev = None
         curr = None
+        prev = None
 
-        # We set first var in stack to be the head so we can instantly pop 
-
-        # Iterate through the stack
         while stack:
-            # Pop the current node from the stack
             curr = stack.pop()
 
-            # Connect the previous node to the current node
             if prev:
-                prev.next = curr
                 curr.prev = prev
+                prev.next = curr
 
-            # Push the next node to the stack if it exists
             if curr.next:
                 stack.append(curr.next)
 
-            # Push the child node to the stack if it exists
             if curr.child:
                 stack.append(curr.child)
                 curr.child = None
-
-            # Move to the next node
+        
             prev = curr
-
+        
         return head
+                
+
+
