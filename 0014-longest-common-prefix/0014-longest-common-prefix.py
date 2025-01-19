12b02b1680ca606a31e84a7001be151e46f262da
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-19 14:46:03 -0500

    Time: 3 ms (29.54%), Space: 18 MB (17.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -8,11 +8,11 @@ class Solution:
         # l 1
         # o 2
         # w 3
-        
+
         for i in range(len(first)):
-            for s in strs:
+            for s in strs[1:]:
                 if i == len(s) or s[i] != first[i]:
                     return res
-            res += s[i]
+            res += first[i]
         return res
             
