9830ae41c035cd3eec2f732d6efc8782794854b2
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-19 17:09:46 -0500

    Time: 549 ms (15.29%), Space: 18.2 MB (46.55%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,19 +1,23 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         def get_power(x):
-            steps = 0
+            counter = 0
             while x != 1:
                 if x % 2 == 0:
                     x = x / 2
                 else:
                     x = 3 * x + 1
-                steps += 1
-            return steps
-            
+                counter += 1
+            return counter
+        
         res = []
         for i in range(lo, hi + 1):
             power = get_power(i)
             res.append([power, i])
         
         res.sort()
-        return res[k - 1][1]
+        return res[k-1][1]
+
+
+        
+
