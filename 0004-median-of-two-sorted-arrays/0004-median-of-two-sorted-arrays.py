40535f0db05d0b7ae6fbfde644b251b008cbf7e0
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-19 14:16:27 -0500

    Time: 1 ms (65.69%), Space: 18.2 MB (23.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -4,7 +4,10 @@ class Solution:
         merged.sort()
         n = len(merged)
 
-        if n % 2 == 0:
-            return (merged[n // 2 - 1] + merged[n // 2]) / 2
-        else:
+        if n % 2 != 0:
             return merged[n // 2]
+        else:
+            return (merged[n // 2] + merged[n // 2 - 1]) / 2
+
+
+            
