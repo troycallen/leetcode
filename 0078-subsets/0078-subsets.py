b057e73189d31183dccaca20234e0000a953cce1
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-21 16:56:54 -0500

    Time: 0 ms (100%), Space: 17.8 MB (40.16%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -7,11 +7,12 @@ class Solution:
             if i >= len(nums):
                 res.append(subset.copy())
                 return
-
+            
             subset.append(nums[i])
             dfs(i + 1)
             subset.pop()
             dfs(i + 1)
-
+        
         dfs(0)
         return res
+        
