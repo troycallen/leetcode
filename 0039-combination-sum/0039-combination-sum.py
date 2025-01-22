9dbdfcb8ee3b38003e1a5c43ded09d070e2baa6e
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-01-21 23:10:36 -0500

    Time: 11 ms (61.57%), Space: 18 MB (24.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,22 +4,21 @@ class Solution:
 
         def dfs(i, cur, total):
             if total == target:
-                # have to make a copy so we dont mess with it in later recursions
                 res.append(cur.copy())
                 return
-            
-            if total > target or i >= len(nums):
-                # if we are out of bounds or too large of a sum
-                return
 
+            if i >= len(nums) or total > target:
+                return
+            
             cur.append(nums[i])
             dfs(i, cur, total + nums[i])
-            # have to pop before we go right in the decision tree
             cur.pop()
-            # cur is now the same as before so we just change i 
             dfs(i + 1, cur, total)
-        
+            
+
         dfs(0, [], 0)
         return res
             
+        
+        
 
