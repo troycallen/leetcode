df08cc9c8b92c9e422517275d25ad1e42d9e4bbd
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-02-20 22:26:52 -0500

    Time: 19 ms (29.68%), Space: 20.1 MB (67.78%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,33 +4,34 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists or len(lists) == 0:
             return None
-
+        
         while len(lists) > 1:
-            mergedLists = []
+            mergedList = []
+
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists = mergedLists
-        return lists[0]
 
+                mergedList.append(self.mergeList(l1, l2))
+
+            lists = mergedList
+        return lists[0]
+        
     def mergeList(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
+        curr = dummy = ListNode()
 
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                curr.next = l1
                 l1 = l1.next
             else:
-                tail.next = l2
+                curr.next = l2
                 l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
+            
+            curr = curr.next
+        
+        curr.next = l1 if l1 else l2
         return dummy.next
