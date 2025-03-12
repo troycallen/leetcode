86843d70aeec4734cd3c82b8c34a758b653cf91d
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-03-12 17:09:02 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,8 +5,10 @@ class Solution:
 
         for i in range(len(first)):
             for s in strs:
-                if s[i] != first[i]:
+                if i == len(s) or s[i] != first[i]:
                     return res
+
             res += first[i]
         
-        return res
+
+            
