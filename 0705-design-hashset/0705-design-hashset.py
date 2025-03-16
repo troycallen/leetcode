749ebed27a110330190b0957aa0d3f003ecfc79c
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-03-16 18:31:12 -0400

    Time: 638 ms (14.07%), Space: 22.2 MB (85.87%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,24 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.map = []
+
+    def add(self, key: int) -> None:
+        if key not in self.map:
+            self.map.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.map:
+            self.map.remove(key)
+
+    def contains(self, key: int) -> bool:
+        if key in self.map:
+            return True
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
