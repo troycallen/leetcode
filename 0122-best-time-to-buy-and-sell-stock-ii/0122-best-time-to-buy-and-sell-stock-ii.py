67a167ce8b9964e5465ea86811ca7eded899a326
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-03-28 23:28:51 -0400

    Time: 0 ms (100%), Space: 19 MB (28.24%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -3,8 +3,9 @@ class Solution:
         profit = 0
 
         for i in range(1, len(prices)):
-            diff = prices[i] - prices[i - 1]
             if prices[i] > prices[i-1]:
-                profit += diff
-        
+                profit += (prices[i] - prices[i-1])
+            
         return profit
+
+            
