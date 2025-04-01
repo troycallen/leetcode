71eeceb3965eeca2bbc2f0d3fdb378842a99fc63
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-04-01 12:11:32 -0400

    Time: 404 ms (95.65%), Space: 20.8 MB (38.96%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,39 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        res = []
 
-        [-4, -1, -1, 0, 1, 2]
-
-        for i,a in enumerate(nums):
-            # It will always be positive solution so we can break
+        for i, a in enumerate(nums):
+            #print(a)
+            # if the first number is greater than 0 we can just break since rest will be > 0
             if a > 0:
                 break
-            
-            # dont use the same element twice
-            if i > 0 and a == nums[i -1]:
+
+            if i > 0 and a == nums[i - 1]:
                 continue
             
-            # need l to be 2nd potential element in the triplet
             l = i + 1
             r = len(nums) - 1
-
             while l < r:
-                three_sum = a + nums[l] + nums[r]
-
-                if three_sum < 0:
-                    l += 1
-                
-                elif three_sum > 0:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
                     r -= 1
-                
+                elif threeSum < 0:
+                    l += 1
                 else:
                     res.append([a, nums[l], nums[r]])
-                    l += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l-1]:
+                    l += 1
+                    while nums[l] == nums[l- 1] and l < r:
                         l += 1
-                
         return res
-                
-
