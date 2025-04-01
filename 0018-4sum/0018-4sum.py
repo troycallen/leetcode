1d9efc6461bb5cbc01059e993db207f97dbbbe1a
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-04-01 12:12:08 -0400

    Time: 362 ms (70.43%), Space: 17.8 MB (58.7%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -6,11 +6,14 @@ class Solution:
         for i, a in enumerate(nums):
             if i > 0 and a == nums[i-1]:
                 continue
+
             for j in range(i + 1, len(nums)):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
+
                 l = j + 1
                 r = len(nums) - 1
+
                 while l < r:
                     four_sum = a + nums[j] + nums[l] + nums[r]
 
@@ -22,10 +25,10 @@ class Solution:
                         res.append([a, nums[j], nums[l], nums[r]])
                         r -= 1
                         l += 1
-                        while l < r and nums[l] == nums[l-1]:
+                        while l < r and nums[l] == nums[l - 1]:
                             l += 1
-                        while r > l and nums[r] == nums[r + 1]:
+                        while l < r and nums[r] == nums[r + 1]:
                             r -= 1
                         
-            return res
+        return res
                             
