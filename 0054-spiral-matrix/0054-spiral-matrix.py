545daf919d4e8de3f1253b1e9a366b55a20251ca
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-07-08 14:34:18 -0400

    Time: 0 ms (100%), Space: 17.7 MB (61.88%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,33 +4,42 @@ class Solution:
 
         if not matrix or not matrix[0]:
             return res
-
-        top = 0
-        bottom = len(matrix) - 1
+        
+        top = 0 
         left = 0
+        bottom = len(matrix) - 1
         right = len(matrix[0]) - 1
 
         while top <= bottom and left <= right:
-
-            for i in range(left, right + 1):
+            for i in range(left, right+1):
                 res.append(matrix[top][i])
-
+            
             top += 1
-
+            
             for i in range(top, bottom + 1):
                 res.append(matrix[i][right])
-
+            
             right -= 1
 
             if top <= bottom:
-                for i in range(right,left -1, -1):
+                for i in range(right, left -1, -1):
                     res.append(matrix[bottom][i])
+                
                 bottom -= 1
-
-            if left <= right:
-                for i in range(bottom,top - 1, -1):
-                    res.append(matrix[i][left])
             
-                left += 1
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                
+                left +=1 
+        
 
         return res
+
+        
+                
+
+                
+
+                
+                
