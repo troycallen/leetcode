f5147def3e20905e1879dc2e4c762c7586290560
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-07-08 13:15:31 -0400

    Time: 35 ms (57.81%), Space: 31.4 MB (49.5%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.py
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.py
@@ -2,18 +2,14 @@ from typing import List
 
 class Solution:
     def maximizeGreatness(self, nums: List[int]) -> int:
-        # Sort original array
         nums.sort()
-        # Make a copy and sort for the permuted array
-        perm = sorted(nums)
-        n = len(nums)
-        i = j = res = 0
-        # Use two pointers to greedily maximize greatness
-        while i < n and j < n:
-            if perm[j] > nums[i]:
-                res += 1
-                i += 1
-                j += 1
-            else:
-                j += 1
-        return res
+        L = 0 
+
+        greatness = 0
+
+        for R in range(len(nums)):
+            if nums[R] > nums[L]:
+                greatness += 1
+                L += 1
+        
+        return greatness
