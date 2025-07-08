b6a461669ed438c68d1fbb6ce11685e3cbde3e67
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-07-08 15:21:39 -0400

    Time: 347 ms (75.99%), Space: 17.7 MB (83.17%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -3,32 +3,31 @@ class Solution:
         nums.sort()
         res = []
 
-        for i, a in enumerate(nums):
+        for i,a in enumerate(nums):
             if i > 0 and a == nums[i-1]:
                 continue
-
-            for j in range(i + 1, len(nums)):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range(i+1, len(nums)):
+                if j > i+1 and nums[j] == nums[j-1]:
                     continue
 
                 l = j + 1
                 r = len(nums) - 1
-
+                
                 while l < r:
                     four_sum = a + nums[j] + nums[l] + nums[r]
-
+                    
                     if four_sum < target:
                         l += 1
                     elif four_sum > target:
                         r -= 1
+
                     else:
                         res.append([a, nums[j], nums[l], nums[r]])
-                        r -= 1
+                        r -=1 
                         l += 1
-                        while l < r and nums[l] == nums[l - 1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r + 1]:
-                            r -= 1
-                        
+                        while l < r and nums[l] == nums[l-1]:
+                            l+=1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -=1 
         return res
-                            
+                 
