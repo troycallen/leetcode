2f25c34ef76ff2c182a581f789736c5fbe60c34d
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-08-10 17:40:23 -0400

    Time: 56 ms (43.56%), Space: 43.5 MB (17.04%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,13 +1,23 @@
 class MyHashMap:
-
     def __init__(self):
         self.map = [-1] * 1000001
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key: int, value: int):
         self.map[key] = value
-
-    def get(self, key: int) -> int:
+    
+    def get(self, key: int):
         return self.map[key]
 
-    def remove(self, key: int) -> None:
+    def remove(self, key: int):
         self.map[key] = -1
+
+        
+
+
+            
+
+
+
+    
+    
+    
