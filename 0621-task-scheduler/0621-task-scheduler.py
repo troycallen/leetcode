ea1381f72ee9063358b9e02495cc9c7f4266fd8a
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-08-26 21:58:51 -0400

    Time: 15 ms (92.83%), Space: 19.2 MB (28.24%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -3,12 +3,16 @@ class Solution:
         count = [0] * 26
         for i in tasks:
             count[ord(i) - ord('A')] += 1
-          
+        
         count.sort()
         maxf = count[25]
         idle = (maxf - 1) * n
 
-        for i in range(24, -1, -1):
-            idle -= min(maxf - 1, count[i])
 
-        return max(idle,0) + len(tasks)
+        for i in range(24, -1, -1):
+            idle -= min((maxf-1, count[i]))
+        
+
+        return max(idle, 0) + len(tasks)
+
+        
