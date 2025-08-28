41fa2378a338ad14e3e4f7b0f4d3d854bce80dc5
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-08-27 22:56:59 -0400

    Time: 0 ms (100%), Space: 18 MB (20.24%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -1,18 +1,16 @@
 class HitCounter:
-
     def __init__(self):
         self.queue = deque()
-
+    
     def hit(self, timestamp: int) -> None:
         self.queue.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
-        while self.queue and timestamp - self.queue[0] >= 300:
+        while self.queue and  timestamp - self.queue[0] >= 300:
             self.queue.popleft()
+    
+        hits = len(self.queue)
 
-        return len(self.queue)
+        return hits
 
-# Your HitCounter object will be instantiated and called as such:
-# obj = HitCounter()
-# obj.hit(timestamp)
-# param_2 = obj.getHits(timestamp)
+        
