c8fff3a3634b149ee9623000e555892ad58ea994
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-08-28 16:41:45 -0400

    Time: 71 ms (88.3%), Space: 19.2 MB (90.75%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -3,13 +3,15 @@ class Solution:
         counts = defaultdict(int)
 
         for i in range(len(s) - minSize + 1):
+            #print(s[i])
+            #counts[s[i]] += 1
             substring = s[i: i + minSize]
-
             if len(set(substring)) <= maxLetters:
                 counts[substring] += 1
             
-            
-        if counts:
-            return max(counts.values())
-        else:
-            return 0
+        
+        return max(counts.values()) if counts else 0
+
+
+        
+        
