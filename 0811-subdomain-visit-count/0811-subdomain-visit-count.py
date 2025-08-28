ff331f5013b4669f6ee899e8b163e9191e017fd3
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-08-28 00:33:50 -0400

    Time: 13 ms (6.36%), Space: 17.9 MB (49.36%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        res = Counter()
+        print(res)
+
+        for domain in cpdomains:
+            count, domain = domain.split()
+            print(count,domain)
+            count = int(count)
+            subs = domain.split('.')
+            print(subs)
+            for i in range(len(subs)):
+                res[".".join(subs[i:])] += count
+        
+        print(res)
+        fmat = ["{} {}".format(c, d) for d, c in res.items()]
+        return fmat
