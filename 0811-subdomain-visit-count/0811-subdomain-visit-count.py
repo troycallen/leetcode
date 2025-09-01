fd87614697e0ff89751faa7d7981a140d2dd2e4f
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-01 16:45:42 -0400

    Time: 11 ms (6.59%), Space: 17.9 MB (50.37%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -1,17 +1,25 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         res = defaultdict(int)
-        print(res)
 
         for domain in cpdomains:
             count, domain = domain.split()
-            print(count,domain)
-            count = int(count)
-            subs = domain.split('.')
-            print(subs)
-            for i in range(len(subs)):
-                res[".".join(subs[i:])] += count
-        
+            #print(count, domain)
+            subdomains = domain.split('.')
+            #print(subdomains)
+            
+            for i in range(len(subdomains)):
+                #print(subdomains[i:])
+                subs = '.'.join(subdomains[i:])
+                #print(subs)
+                res[subs] += int(count)
+            
         print(res)
-        fmat = ["{} {}".format(c, d) for d, c in res.items()]
-        return fmat
+        result = []
+        for key, val in res.items():
+            print(key,val)
+            result.append(f"{val} {key}")
+        
+        return result
+        
+        
