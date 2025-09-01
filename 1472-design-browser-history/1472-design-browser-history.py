15a1b0fb1bf64bd84e97e6e83c0bcdbac21fa637
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-01 18:08:09 -0400

    Time: 59 ms (17.83%), Space: 20.4 MB (20.71%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -15,17 +15,14 @@ class BrowserHistory:
     def back(self, steps: int) -> str:
         while self.cur.prev and steps > 0:
             self.cur = self.cur.prev
-            steps -= 1
+            steps -=1 
         return self.cur.val
 
     def forward(self, steps: int) -> str:
         while self.cur.next and steps > 0:
             self.cur = self.cur.next
-            steps -=1 
+            steps -=1
         return self.cur.val
-
-            
-
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
