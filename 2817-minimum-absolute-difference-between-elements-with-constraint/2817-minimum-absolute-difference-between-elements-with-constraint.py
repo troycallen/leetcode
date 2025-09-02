d6b9f04e33d1f31ab06890d996a5d999505f2fac
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-02 19:19:24 -0400

    Time: 738 ms (36.75%), Space: 51.8 MB (11.8%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
@@ -1,23 +1,22 @@
 class Solution:
     def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
-        nums = sorted((nums[i], i) for i in range(len(nums)))
-        print(nums)
+
+        sortedNums = sorted((val, i) for i, val in enumerate(nums))
+
         heapLeft = []
         heapRight = []
+        minDiff = float('inf')
 
-        minDiff = 99999999999
-
-        for i in range(len(nums)):
-            val, index = nums[i]
-
+        for val, index in sortedNums:
             heapq.heappush(heapLeft, (index, val))
             heapq.heappush(heapRight, (-index, val))
-
+        
             while heapLeft and heapLeft[0][0] + x <= index:
                 minDiff = min(minDiff, val - heapq.heappop(heapLeft)[1])
-
+            
             while heapRight and heapRight[0][0] + x <= -index:
                 minDiff = min(minDiff, val - heapq.heappop(heapRight)[1])
             
         return minDiff
 
+        
