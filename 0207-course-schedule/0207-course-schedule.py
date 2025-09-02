e1818796bb10d4160610738d7462598dc235d532
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-01 22:18:08 -0400

    Time: 7 ms (46.11%), Space: 19.9 MB (10.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = {}
-        for i in range(numCourses):
-            adj[i] = []
+        adj = defaultdict(list)
         for a,b in prerequisites:
             adj[a].append(b)
-    
+
         cycle = set()
         seen = set()
 
         def dfs(cur):
             if cur in cycle:
                 return False
+            
             if cur in seen:
                 return True
-
+                
             cycle.add(cur)
+            
             for child in adj[cur]:
                 if not dfs(child):
                     return False
+                
             cycle.remove(cur)
-
             seen.add(cur)
-            return True
 
+            return True
+        
         for i in range(numCourses):
             if not dfs(i):
                 return False
-
-        return True 
+        
+        return True
 
         
