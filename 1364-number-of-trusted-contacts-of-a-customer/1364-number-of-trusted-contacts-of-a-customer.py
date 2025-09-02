3155ff29eded832e4263294c0a6e1c76147bc839
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-02 18:15:10 -0400

    Time: 396 ms (89.13%), Space: 69.7 MB (71.74%) - LeetHub

--- 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.py
+++ 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.py
@@ -1,14 +1,11 @@
 import pandas as pd
 
 def count_trusted_contacts(customers: pd.DataFrame, contacts: pd.DataFrame, invoices: pd.DataFrame) -> pd.DataFrame:
-    # 1. merge customer and contacts
-    contact_customer = contacts.merge(customers, how = 'left', left_on = 'contact_email', right_on = 'email') 
-    # if email is null means this contact is NOT customer
-    # 2. count contacts and trusted contacts
-    contact_customer = contact_customer.groupby('user_id').agg(contacts_cnt = ('contact_email', 'count'), trusted_contacts_cnt = ('email', 'count'))
-    # 3. merge invoices with customer to get names
+
+    contact_customer = contacts.merge(customers, how = 'left', left_on = 'contact_email', right_on = 'email')
+    contact_customer = contact_customer.groupby('user_id').agg(contacts_cnt = ('contact_email', 'count'), 
+        trusted_contacts_cnt = ('email', 'count'))
     invoices = invoices.merge(customers, how = 'left', left_on = 'user_id', right_on = 'customer_id')
-    # 4. merge invoices with contact_customer to get counts, fillna if null
-    df = invoices.merge(contact_customer, how = 'left', on = 'user_id').fillna(0)
-    # 5. get wanted columns and reorder
-    return df[['invoice_id', 'customer_name', 'price', 'contacts_cnt', 'trusted_contacts_cnt']].sort_values(by = 'invoice_id', ascending = True)
+    df = invoices.merge(contact_customer, how = 'left', on = 'user_id')
+    df = df.fillna(0)
+    return df[['invoice_id', 'customer_name', 'price', 'contacts_cnt', 'trusted_contacts_cnt']].sort_values(by = 'invoice_id')
