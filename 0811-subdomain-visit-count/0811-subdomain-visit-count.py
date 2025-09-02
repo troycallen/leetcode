73af781b28c98c635f068e6d642d8a54684a230e
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-01 23:19:49 -0400

    Time: 15 ms (6.59%), Space: 18.2 MB (10.76%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -4,22 +4,19 @@ class Solution:
 
         for domain in cpdomains:
             count, domain = domain.split()
-            #print(count, domain)
-            subdomains = domain.split('.')
-            #print(subdomains)
-            
+            subdomains = domain.split(".")
+            print(count, subdomains)
+
             for i in range(len(subdomains)):
-                #print(subdomains[i:])
-                subs = '.'.join(subdomains[i:])
-                #print(subs)
+                subs = ".".join(subdomains[i:])
+                print(subs)
+                # have to do add for dupes of int counts like com, etc
                 res[subs] += int(count)
             
         print(res)
-        result = []
-        for key, val in res.items():
-            print(key,val)
-            result.append(f"{val} {key}")
+        res_2 = []
+        for domain, count in res.items():
+            res_2.append(f"{count} {domain}")
         
-        return result
-        
-        
+        return res_2
+                    
