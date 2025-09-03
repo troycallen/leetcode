3eb9fabafd3dc3c505e5da388b26c27248ec5ae7
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-03 00:12:09 -0400

    Time: 126 ms (62.23%), Space: 34.5 MB (11.27%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.py
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.py
@@ -2,19 +2,20 @@ class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         freq = defaultdict(int)
         l = 0
-        pairs = 0
         res = 0
+        pairs = 0
 
         for r in range(len(nums)):
             num = nums[r]
             pairs += freq[num]
             freq[num] += 1
-            
+
             while pairs >= k:
                 res += len(nums) - r
-                remove = nums[l]
-                freq[remove] -= 1
-                pairs -= freq[remove]
+                removed_num = nums[l]
+                freq[removed_num] -= 1
+                pairs -= freq[removed_num]
                 l += 1
+    
         return res
 
