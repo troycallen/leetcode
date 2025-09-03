c473add84306a69d2ccd75c918925c567e6042dd
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-03 16:40:04 -0400

    Time: 43 ms (28.85%), Space: 18.8 MB (26.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,10 +3,8 @@ class Solution:
         max_reach = 0
 
         for i in range(len(nums)):
-            
             if i > max_reach:
                 return False
+            max_reach = max(max_reach, i + nums[i])
 
-            max_reach = max(max_reach, nums[i] + i)
-            
         return True
