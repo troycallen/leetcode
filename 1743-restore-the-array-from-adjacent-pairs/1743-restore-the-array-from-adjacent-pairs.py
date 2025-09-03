a9c9d28efdf851da36a77dcf507b09656760b73f
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-02 20:37:01 -0400

    Time: 125 ms (86.68%), Space: 58.8 MB (88.18%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -1,27 +1,34 @@
 class Solution:
     def restoreArray(self, pairs: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
+
         for a,b in pairs:
             graph[a].append(b)
             graph[b].append(a)
-        
-        res = []
 
-        for node, neighbor in graph.items():
+        res = []
+        
+        for node,neighbor in graph.items():
             if len(neighbor) == 1:
                 res = [node, neighbor[0]]
         
+        print(res)
         while len(res) < len(pairs) + 1:
             last = res[-1]
-            prev = res[-2]
+            second_to_last = res[-2]
 
             candidates = graph[last]
 
-            next_ele = candidates[0] if candidates[0] != prev else candidates[1]
+            next_element = candidates[1] if candidates[1] != second_to_last else candidates[0]
 
-            res.append(next_ele)
-        
+            res.append(next_element)
+    
         return res
+        
+
+            
+        
+        
 
         
 
