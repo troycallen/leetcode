2f78f532aaf373139affb27d11b0bc43c16c9e56
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-03 16:05:07 -0400

    Time: 3 ms (64.19%), Space: 17.8 MB (93.2%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,14 +1,13 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         groups = defaultdict(list)
-        result = []
-        
+        res = []
+
         for person, size in enumerate(groupSizes):
+
             groups[size].append(person)
-            
-            # When we have enough people for a complete group
             if len(groups[size]) == size:
-                result.append(groups[size])
-                groups[size] = []  # Start new group
+                res.append(groups[size])
+                groups[size] = []
         
-        return result
+        return res
