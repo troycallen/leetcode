de2b6e9064143bf0591790e79d8ba03b9c018ee8
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-02 20:57:50 -0400

    Time: 151 ms (63.04%), Space: 58.8 MB (96.62%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -5,26 +5,25 @@ class Solution:
         for a,b in pairs:
             graph[a].append(b)
             graph[b].append(a)
-
+        
         res = []
+
+        for node, neighbors in graph.items():
+            if len(neighbors) == 1:
+                res = [node, neighbors[0]]
         
-        for node,neighbor in graph.items():
-            if len(neighbor) == 1:
-                res = [node, neighbor[0]]
-        
-        print(res)
         while len(res) < len(pairs) + 1:
             last = res[-1]
-            second_to_last = res[-2]
+            prev = res[-2]
 
             candidates = graph[last]
 
-            next_element = candidates[1] if candidates[1] != second_to_last else candidates[0]
-
+            next_element = candidates[1] if candidates[1] != prev else candidates[0]
+            
             res.append(next_element)
-    
-        return res
         
+        return res
+
 
             
         
