339511237b8de7f325addf64870414972af3823c
Troy Allen <165961938+troycallen@users.noreply.github.com>
2025-09-02 23:53:06 -0400

    Time: 538 ms (5.11%), Space: 63.6 MB (43.37%) - LeetHub

--- 2021-brightest-position-on-street/2021-brightest-position-on-street.py
+++ 2021-brightest-position-on-street/2021-brightest-position-on-street.py
@@ -2,22 +2,23 @@ class Solution:
     def brightestPosition(self, lights: List[List[int]]) -> int:
         events = []
 
-        for position, rangeVal in lights:
-            start = position - rangeVal
-            end = position + rangeVal + 1
+        for pos, val in lights:
+            start = pos - val
+            end = pos + val + 1
             events.append((start, 1))
             events.append((end, -1))
         
         events.sort()
-        currBrightness = 0
-        maxBrightness = 0
-        bestPos = 0
+        currB, maxB, pos = 0, 0, 0
 
-        for pos, change in events:
-            print(pos, change)
+        print(events)
 
-            currBrightness += change
-            if currBrightness > maxBrightness:
-                currBrightness = maxBrightness
-                bestPos = pos
-        return bestPos
+        for position, change in events:
+            print(position,change)
+            currB += change
+            if currB > maxB:
+                currB = maxB
+                pos = position
+        
+        return pos
+            
